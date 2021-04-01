f3493bb1dc201d840a47c333d6c64087fd5cbe1f
Stephen N <sndegwa.n@outlook.com>
2021-04-01 19:17:46 +0300

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import React from "react";
+
+import { addDecorator } from "@storybook/react";
+
+import { MemoryRouter } from "react-router-dom";
+
+import '../src/styles/base.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+addDecorator(story => <MemoryRouter initialEntries={['/']} > { story() } </MemoryRouter>);

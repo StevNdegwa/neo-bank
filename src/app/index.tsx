141815ba2db65c6c362f414798fb0ce64ce284f2
Stephen N <sndegwa.n@outlook.com>
2021-04-05 09:47:43 +0300

    chore: add react-icons provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { IconContext } from "react-icons";
 import Home from "./components/Home";
 import Auth from "./components/Auth";
 
 function App() {
   return (
-    <Router>
+    <IconContext.Provider value={{className:"neo-bank-react-icons"}}>
+      <Router>
       <Switch>
         <Route path="/create-acount" component={Auth.CreateAccount}/>
         <Route path="/retail-login" component={Auth.Login}/>
         <Route path="/" component={Home}/>  
       </Switch> 
     </Router>
+    </IconContext.Provider>
   );
 }
 

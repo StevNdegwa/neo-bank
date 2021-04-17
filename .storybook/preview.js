a33be68d513312480e1185b039f0babf76de062a
Stephen N <sndegwa.n@outlook.com>
2021-04-17 20:39:19 +0300

    docs:update storybook configurations

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,30 @@
-import React from "react";
-
-import { addDecorator } from "@storybook/react";
+import React, { Suspense } from "react";
 
 import { MemoryRouter } from "react-router-dom";
+import { RelayEnvironmentProvider } from "react-relay";
+import { IconContext } from "react-icons";
 
+import FirebaseContext from "../src/app/FirebaseContext";
+import Firebase from "../src/utils/firebase";
+import environment from "../src/app/config/relay/Environment";
 import '../src/styles/all.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-addDecorator(story => <MemoryRouter initialEntries={['/']} > { story() } </MemoryRouter>);
+export const decorators = [
+  (Story) => (
+    <RelayEnvironmentProvider environment={environment}>
+      <IconContext.Provider value={{ className: "neo-bank-react-icons" }}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <MemoryRouter initialEntries={['/']} >
+            <Suspense fallback={<div>loading...</div>}>
+            <Story/>
+            </Suspense>
+          </MemoryRouter>
+        </FirebaseContext.Provider>
+      </IconContext.Provider>
+    </RelayEnvironmentProvider>
+  )  
+]
